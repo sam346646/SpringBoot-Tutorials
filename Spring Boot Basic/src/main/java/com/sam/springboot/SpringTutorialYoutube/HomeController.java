6c51c3389db8d35a7b9d30c6f8ae47d2ac8da9f6
sam346646 <sammanasseh310@gmail.com>
2023-09-10 16:05:30 +0530

    Thymaleaf Html updated

--- Spring Boot Basic/src/main/java/com/sam/springboot/SpringTutorialYoutube/HomeController.java
+++ Spring Boot Basic/src/main/java/com/sam/springboot/SpringTutorialYoutube/HomeController.java
@@ -1,13 +1,22 @@
 package com.sam.springboot.SpringTutorialYoutube;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
+//public class HomeController {
+//	@RequestMapping(value = "/")
+//	public static String Welcome() {
+//		return "Welcome to springboot tutorial \n" +
+//				"This is Home controller";
+//	}
+//}
+
+@Controller
 public class HomeController {
 	@RequestMapping(value = "/")
 	public static String Welcome() {
-		return "Welcome to springboot tutorial \n" +
-				"This is Home controller";
+		return "index";
 	}
 }
