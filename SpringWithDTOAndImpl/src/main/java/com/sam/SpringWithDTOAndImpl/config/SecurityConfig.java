344516aaa852be01fe65df7bfc830c67714514fe
sam346646 <sammanasseh310@gmail.com>
2023-09-12 20:48:06 +0530

    spring-dto: In memory Authentication updated with spring 6

--- SpringWithDTOAndImpl/src/main/java/com/sam/SpringWithDTOAndImpl/config/SecurityConfig.java
+++ SpringWithDTOAndImpl/src/main/java/com/sam/SpringWithDTOAndImpl/config/SecurityConfig.java
@@ -4,18 +4,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
+	
+	@Bean
+	public InMemoryUserDetailsManager userDetailsService() {
+		UserDetails user=User
+				.withDefaultPasswordEncoder()
+				.username("user")
+				.password("pass")
+				.roles("USER")
+				.build();
+		return new InMemoryUserDetailsManager(user);
+	}
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 		.authorizeHttpRequests((auth) -> auth
 			.requestMatchers("/employee/all","/employee/byId/*","/employee/byName/*").permitAll()
-			.requestMatchers("/employee/delete/*").hasRole("ADMIN")
+			.requestMatchers("/employee/delete/*").hasRole("USER")
 			.anyRequest().authenticated())
 		.formLogin();
 		
