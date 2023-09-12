853ab0bf8f578ba2cc367ffb79e8f5d79fc9d431
sam346646 <sammanasseh310@gmail.com>
2023-09-12 14:40:23 +0530

    spring-dto: Security config implemented(api access handled by login or full access to view only)

---
+++ SpringWithDTOAndImpl/src/main/java/com/sam/SpringWithDTOAndImpl/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.sam.SpringWithDTOAndImpl.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+		.authorizeHttpRequests((auth) -> auth
+		.requestMatchers("/employee/all","/employee/byId/*","/employee/byName/*")
+		.permitAll()
+		.anyRequest()
+		.authenticated())
+		.formLogin();
+		
+		return http.build();
+	}
+}
