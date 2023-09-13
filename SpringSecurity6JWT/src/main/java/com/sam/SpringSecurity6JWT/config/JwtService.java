a5ca598eb987c771af34f7c447fb1b1da844e645
sam346646 <sammanasseh310@gmail.com>
2023-09-13 19:04:38 +0530

    spring-security6-jwt: JwtService updated with generate and validate token

--- SpringSecurity6JWT/src/main/java/com/sam/SpringSecurity6JWT/config/JwtService.java
+++ SpringSecurity6JWT/src/main/java/com/sam/SpringSecurity6JWT/config/JwtService.java
@@ -1,12 +1,17 @@
 package com.sam.SpringSecurity6JWT.config;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
@@ -36,6 +41,36 @@ public class JwtService {
 	private Key getSignInKey() {
 		final byte[] keyBytes=Decoders.BASE64.decode(secret_key);
 		return Keys.hmacShaKeyFor(keyBytes);
+	}
+	
+	
+	
+	private String generateToken(UserDetails userDetails) {
+		return generateToken(new HashMap<>(), userDetails);
+	}
+
+	private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+		return Jwts
+			   .builder()
+			   .setClaims(extraClaims)
+			   .setSubject(userDetails.getUsername())
+			   .setIssuedAt(new Date(System.currentTimeMillis()))
+			   .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
+			   .signWith(getSignInKey(),SignatureAlgorithm.HS256)
+			   .compact();
+	}
+	
+	public boolean isTokenValid(String token, UserDetails userDetails) {
+		String userEmail= extractUsername(token);
+		return (userEmail.equals(userDetails.getUsername())) && !isTokenExpired(token); 
+	}
+
+	private boolean isTokenExpired(String token) {
+		return extractExporation(token).before(new Date());
+	}
+
+	private Date extractExporation(String token) {
+		return extractClaim(token, Claims::getExpiration);
 	}
 	
 }
