2f2814e7c305b406723652dfe756d405cb265202
sam346646 <sammanasseh310@gmail.com>
2023-09-13 14:11:33 +0530

    spring-security6-jwt: user,user repo and application.proprties updated

---
+++ SpringSecurity6JWT/src/main/java/com/sam/SpringSecurity6JWT/SpringSecurity6JwtApplication.java
@@ -0,0 +1,13 @@
+package com.sam.SpringSecurity6JWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity6JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity6JwtApplication.class, args);
+	}
+
+}
---
+++ SpringSecurity6JWT/src/main/java/com/sam/SpringSecurity6JWT/user/User.java
@@ -0,0 +1,28 @@
+package com.sam.SpringSecurity6JWT.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String firstname;
+	private String lastname;
+	private String email;
+	private String password;
+	
+}
