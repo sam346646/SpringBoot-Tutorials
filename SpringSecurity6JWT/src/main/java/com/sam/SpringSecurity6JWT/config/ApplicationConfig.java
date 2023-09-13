ab0c4f465e16f4c93a5ce239b8edf3c54aaa754a
sam346646 <sammanasseh310@gmail.com>
2023-09-13 20:20:31 +0530

    spring-security6-jwt: Applicationconfig updated with Authentication Provide and manager

--- SpringSecurity6JWT/src/main/java/com/sam/SpringSecurity6JWT/config/ApplicationConfig.java
+++ SpringSecurity6JWT/src/main/java/com/sam/SpringSecurity6JWT/config/ApplicationConfig.java
@@ -3,8 +3,14 @@ package com.sam.SpringSecurity6JWT.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.sam.SpringSecurity6JWT.user.UserRepository;
 
@@ -24,4 +30,21 @@ public class ApplicationConfig {
 		
 	}
 	
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider= new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+		
+		return authProvider;
+	}
+
+	private PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		 	return config.getAuthenticationManager();
+	}
 }
