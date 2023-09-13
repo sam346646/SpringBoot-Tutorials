dd3f9c168e4f14f0ebb5efdf99525de4027e4fc1
sam346646 <sammanasseh310@gmail.com>
2023-09-13 20:04:31 +0530

    spring-security6-jwt: Applicationconfig updated

--- SpringSecurity6JWT/src/main/java/com/sam/SpringSecurity6JWT/user/UserRepository.java
+++ SpringSecurity6JWT/src/main/java/com/sam/SpringSecurity6JWT/user/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 	
-	Optional<User> fingByEmail(String email);
+	Optional<User> findByEmail(String email);
 
 }
